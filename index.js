0340de946abf161c0b70c0ee74fa9e314d6b698c
SOFIASEMCHUK <semchuk.s02@gmail.com>
2022-12-03 13:38:06 +0200

    add js file 7-8 line

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const operator = prompt('Введіть оператор: +, -, *, /');
-const firstOperand = prompt('Ввведіть перше число');
-const secondOperand = prompt('Ввведіть друге число');
+const firstOperand = +prompt('Ввведіть перше число');
+const secondOperand = +prompt('Ввведіть друге число');
 
-let result = `${Number(firstOperand) operator Number(secondOperand)}`;
+function calc(operator, firstOperand, secondOperand); { 
+    if (operator === '+'){
+        alert(`${firstOperand} ${operator} ${secondOperand} = ${firstOperand + secondOperand}`);
+    } 
+    // else if (operator === '-'){
+    //     alert(`${firstOperand} - ${secondOperand} = ${firstOperand - secondOperand}`); 
+    // } else if (operator === '*'){
+    //     alert(`${firstOperand} * ${secondOperand} = ${firstOperand * secondOperand}`); 
+    // } else (operator === '/');{
+    //     alert(`${firstOperand} / ${secondOperand} = ${firstOperand / secondOperand}`); 
+    // }
+}
 
-alert(`${firstOperand} ${operator} ${secondOperand} = ${result}`);
+// let result = `${Number(firstOperand) operator Number(secondOperand)}`;
+
+// alert(`${firstOperand} ${operator} ${secondOperand} = ${result}`);
 
 
 //`${firstOperand} ${operator} ${secondOperand}`
