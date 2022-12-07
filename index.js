03753d5055026a05c91d1626616bbd80441c8a51
SOFIASEMCHUK <semchuk.s02@gmail.com>
2022-12-07 19:46:56 +0200

    correct js file

--- index.js
+++ index.js
@@ -2,23 +2,14 @@ const operator = prompt('Введіть оператор: +, -, *, /');
 const firstOperand = +prompt('Ввведіть перше число');
 const secondOperand = +prompt('Ввведіть друге число');
 
-function calc(operator, firstOperand, secondOperand); { 
-    if (operator === '+'){
-        alert(`${firstOperand} ${operator} ${secondOperand} = ${firstOperand + secondOperand}`);
-    } 
-    // else if (operator === '-'){
-    //     alert(`${firstOperand} - ${secondOperand} = ${firstOperand - secondOperand}`); 
-    // } else if (operator === '*'){
-    //     alert(`${firstOperand} * ${secondOperand} = ${firstOperand * secondOperand}`); 
-    // } else (operator === '/');{
-    //     alert(`${firstOperand} / ${secondOperand} = ${firstOperand / secondOperand}`); 
-    // }
-}
-
-// let result = `${Number(firstOperand) operator Number(secondOperand)}`;
-
-// alert(`${firstOperand} ${operator} ${secondOperand} = ${result}`);
-
-
-//`${firstOperand} ${operator} ${secondOperand}`
-
+if(operator === '+'){
+    alert(`${firstOperand} ${operator} ${secondOperand} = ${firstOperand + secondOperand}`);
+}else if(operator === '-'){
+    alert(`${firstOperand} ${operator} ${secondOperand} = ${firstOperand - secondOperand}`); 
+}else if(operator === '*'){
+    alert(`${firstOperand} ${operator} ${secondOperand} = ${firstOperand * secondOperand}`); 
+}else if(operator === '/'){
+    alert(`${firstOperand} ${operator} ${secondOperand} = ${firstOperand / secondOperand}`); 
+}else{
+    alert('Спробуй ще');
+}   
